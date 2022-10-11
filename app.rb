38616131887158471567ac661c96e8868f37f7af
Alex-Skripnik <Skripnik.O.Olex@gmail.com>
2022-10-11 14:13:29 +0300

    Add /vizit + js plugins

--- app.rb
+++ app.rb
@@ -11,9 +11,33 @@ end
 class Barber < ActiveRecord::Base
 end
 
-get '/' do
-  @barbers = Barber.order "created_at DESC"
-  erb :index
-
+before do
+  @barbers = Barber.all
 end
+
+get '/' do
+  
+  erb :index
+end
+
+get '/vizit' do
+  erb :vizit
+end
+
+# спросим Имя, номер телефона и дату, когда придёт клиент.
+post '/vizit' do
+  # user_name, phone, date_time
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
+
+  
+  @title = "Thank you!"
+  @message = "Уважаемый #{@username}, мы ждём вас #{@datetime} у выбранного парикмахера #{@barber}."
+
+  erb :message
+end
+
 
