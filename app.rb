4a39cef56f036110e9466e087088a8b8ebc74b58
Alex-Skripnik <Skripnik.O.Olex@gmail.com>
2022-10-11 15:03:19 +0300

    Add message.erb and update app.rb - post'/vizit'

--- app.rb
+++ app.rb
@@ -33,6 +33,17 @@ post '/vizit' do
   @barber = params[:barber]
   @color = params[:color]
 
+  # name, phone, datestamp, barber, color
+
+  c = Client.new 
+  c.name = @username
+  c.phone = @phone
+  c.datestamp = @datetime
+  c.barber = @barber
+  c.color = @color
+  c.save
+  
+
   
   @title = "Thank you!"
   @message = "Уважаемый #{@username}, мы ждём вас #{@datetime} у выбранного парикмахера #{@barber}."
